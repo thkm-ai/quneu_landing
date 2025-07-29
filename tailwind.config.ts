5d6d0747f3697feb67396846a84eff66ab3e101f
praveen <cardinalgracias31@gmail.com>
2025-07-29 11:20:11 -0700

    br

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   ],
   theme: {
   	extend: {
-  		colors: {
+  		colors: { 
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
